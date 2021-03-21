fe1bcaa823d024e2c2f2beb430b4e1a5f207f4f0
Johan Larsson <johan.larsson2@sweco.se>
2021-03-21 18:17:39 +0100

    Changed repo

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <>
-      <footer className={styles.footer}>footer</footer>
+      <footer className={styles.footer}>footer dd</footer>
     </>
   );
 }
